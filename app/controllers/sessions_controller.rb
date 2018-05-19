c46a23cf747422c58665fb7df1e8795be2cd55d2
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-05-18 18:57:55 -0700

    began css styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,24 @@ class SessionsController < ApplicationController
     def new 
         @user = User.new
     end
-    
+
     def create
-      @user = User.find_or_create_by(uid: auth['uid']) do |u|
-        u.name = auth['info']['name']
-        u.email = auth['info']['email']
-        u.image = auth['info']['image']
-      end
-   
-      session[:user_id] = @user.id
-   
-      render 'welcome/home'
+        if @user = User.find_by(name: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to root_path, notice: "Welcome back to the theme park!"
+            else
+                render ':sessions/new'
+            end
+        elsif params[:auth][:uid]
+            @user = User.find_or_create_by(uid: auth['uid']) do |u|
+                u.name = auth['info']['name']
+                u.email = auth['info']['email']
+                u.image = auth['info']['image']
+                end
+                session[:user_id] = @user.id
+                render ':welcome/index'
+        end
     end
 
     def destroy
@@ -26,4 +33,5 @@ class SessionsController < ApplicationController
     def auth
       request.env['omniauth.auth']
     end
+
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    
     has_secure_password
     has_many :recipes
     has_many :meals
