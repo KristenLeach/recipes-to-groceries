8735e4945f39baa4fe26ac791700ac7104c91b7f
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-07-23 22:05:47 -0700

    Finished ajax for three index pages, added heart/mealplan forms to html

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,14 +1,27 @@
 $(function(){
     $(".indexPages").click(function(e){
         e.preventDefault()
+        const defaultTitle = "<h3> Check out these great recipes! </h3>"
+        let title;
+        switch(this.text) {
+            case 'Popular Recipes':
+                title = "<h3> Popular Recipes </h3><h4> Here's what people are meal-planning this week: </h4>";
+                break;
+            case 'Recipes you Love':
+                title = '<h3>Recipes you Love</h3>';
+                break;
+            default:
+                title = defaultTitle;
+        };
         $.getJSON(this.href).success(function(data){
-            const recipes = data.map( recipeJSON => {
-                //debugger
+            data.length === 0 ? title = "<h3> No Recipes to Show </h3>" : title
+
+            const recipes = data.map( recipeJSON => { 
                 const recipeObj = new Recipe(recipeJSON)
-                
                 return recipeObj.renderCard()
             })
-            $('.content').html(`<div class="recipe-container">${recipes.join('')}</div>`)
+            $('.content').html(`${title}<div class="recipe-container">${recipes.join('')}</div>`)
+            attachListeners()
         })
     })
     
@@ -20,12 +33,24 @@ $(function(){
         this.userImage = json.user.image.image.thumb.url
         this.userName = json.user.name
         this.userId = json.user.id
-        //debugger
+        this.heart = json.hearts[0]
     }
 
     Recipe.prototype.renderCard = function() {
-        var AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
+        let AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
             console.log(AUTH_TOKEN)
+        const hearted = `<form class="button_to" method="delete" action="/unheart?recipe_id=${this.id}">
+                <input type="hidden" name="authenticity_token" value="${AUTH_TOKEN}">
+                <button class="liked" type="submit">
+                <i class="fas fa-heart" id="recipe_${this.id}"></i>
+                </button>
+                </form>`
+        const unhearted = `<form class="button_to" method="post" action="/heart?recipe_id=${this.id}">
+                <input type="hidden" name="authenticity_token" value="${AUTH_TOKEN}">
+                <button class="liked" type="submit">
+                <i class="far fa-heart" id="recipe_${this.id}"></i>
+                </button>
+                </form>`
         return `<div class='recipe-card'>
         <img src= ${this.image} class='recipe-img'>
         <a href="/recipes/${this.id}" class='name'>${this.name}</a>
@@ -42,12 +67,7 @@ $(function(){
         <img src= ${this.userImage} class='user-img'>
         <small class='user-link'><a href="/users/${this.userId}">${this.userName}</a></small>
         <div class='liked'>
-            <form class="button_to" method="post" action="/heart?recipe_id=${this.id}">
-                <input type="hidden" name="authenticity_token" value="${AUTH_TOKEN}">
-                <button class="liked" type="submit">
-                <i class="far fa-heart" id="recipe_2"></i>
-                </button>
-            </form>
+            ${this.heart ? hearted : unhearted }
         </div>
         </div>`
   }
